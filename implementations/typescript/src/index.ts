0bf0288b1d34305edb4ef46366c12022325fa173
Serhii Ponomarov <sergii.ponomarov@gmail.com>
2026-08-24 20:31:51 +0300

    Export control mapping API

--- implementations/typescript/src/index.ts
+++ implementations/typescript/src/index.ts
@@ -2,6 +2,7 @@ export * from "./types.js";
 export * from "./assessment-types.js";
 export * from "./assessment-diff.js";
 export * from "./remediation.js";
+export * from "./control-mapping.js";
 export * from "./validate.js";
 export * from "./normalize.js";
 export * from "./redact.js";
