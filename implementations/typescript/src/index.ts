87861ded9774411e2786e30d9b23023860616428
Serhii Ponomarov <sergii.ponomarov@gmail.com>
2026-08-24 19:55:19 +0300

    Export assessment diff APIs

--- implementations/typescript/src/index.ts
+++ implementations/typescript/src/index.ts
@@ -1,5 +1,6 @@
 export * from "./types.js";
 export * from "./assessment-types.js";
+export * from "./assessment-diff.js";
 export * from "./validate.js";
 export * from "./normalize.js";
 export * from "./redact.js";
